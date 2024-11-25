51c92ffe398f07ed1b208bd4483f1cfa9e72c217
Neo-Diamons <romain.hure@epitech.eu>
2024-11-25 16:17:24 +0100

    feat(auth): add encryption for password.

--- app/controllers/auth.ts
+++ app/controllers/auth.ts
@@ -2,20 +2,23 @@ import { Context } from "@hono";
 import { sign } from "@hono/jwt";
 import { db } from "../db/config.ts";
 import { users as userSchema } from "../schemas/users.ts";
-import { and, eq } from "drizzle-orm/expressions";
+import { eq } from "drizzle-orm/expressions";
+import bcrypt from "bcrypt";
+
+if (!Deno.env.get("SALT_ROUNDS")) {
+  console.error("Please set the SALT_ROUNDS environment variable");
+  Deno.exit(1);
+}
 
 async function login(ctx: Context) {
   // @ts-ignore - The `form` validator is added by the `validator` middleware
   const { email, password } = ctx.req.valid("form");
 
   const users = await db.select().from(userSchema).where(
-    and(
-      eq(userSchema.email, email),
-      eq(userSchema.password, password),
-    ),
+    eq(userSchema.email, email),
   ).limit(1);
 
-  if (!users.length) {
+  if (!users.length || !(await bcrypt.compare(password, users[0].password))) {
     return ctx.json({ message: "Invalid email or password" }, 401);
   }
 
@@ -43,7 +46,16 @@ async function register(ctx: Context) {
     return ctx.json({ message: "User already exists" }, 400);
   }
 
-  await db.insert(userSchema).values({ email, username, password });
+  const hashedPassword = await bcrypt.hash(
+    password,
+    parseInt(Deno.env.get("SALT_ROUNDS")!),
+  );
+
+  await db.insert(userSchema).values({
+    email,
+    username,
+    password: hashedPassword,
+  });
   return ctx.json({ email, username });
 }
 
