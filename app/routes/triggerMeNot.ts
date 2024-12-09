592cdab238cdf438e31433d343873d312c50a3f6
Romain <romain.hure@epitech.eu>
2024-12-09 02:30:04 +0100

    fix(app/routes/triggerMeNot): remove authorization.

--- app/routes/triggerMeNot.ts
+++ app/routes/triggerMeNot.ts
@@ -1,13 +1,10 @@
 import { Hono } from "@hono";
 import { describeRoute } from "@hono-openapi";
 import { validator } from "@hono-openapi/zod";
-import jwtMiddleware from "../middlewares/jwt.ts";
 import triggerMeNotController from "../controllers/triggerMeNot.ts";
 import triggerMeNotSchema from "../interfaces/triggerMeNot.ts";
 
 const triggerMeNotRouter = new Hono();
-
-triggerMeNotRouter.use(jwtMiddleware);
 
 triggerMeNotRouter.post(
   "/on-fetch/:token",
